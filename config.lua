a160a88444dc513cb49ac21cbe3c14232d8f23e9
Aqua Development <anonymanonym1565@gmail.com>
2025-02-15 20:12:06 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+
+Config.LockKey = 303 
+
+
+Config.LockDistance = 30.0 
+
+
+Config.AnimationDict = "anim@mp_player_intmenu@key_fob@"
+Config.AnimationName = "fob_click"
+
+
+Config.LockNotify = "🔒 Fahrzeug abgeschlossen"
+Config.UnlockNotify = "🔓 Fahrzeug geöffnet"
+Config.OutOfRangeNotify = "⚠ Du bist zu weit entfernt vom Fahrzeug!"
