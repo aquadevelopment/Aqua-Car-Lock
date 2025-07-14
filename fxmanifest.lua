208562c91c9e64da20363ec072a53986e0fa0665
Aqua Development <anonymanonym1565@gmail.com>
2025-07-14 20:24:02 +0200

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Aqua Development' 
-description 'Ein Car Lock System für deinen FiveM Server'
-version '1.0.0'
-
-client_script 'client.lua'
-shared_script 'config.lua'
